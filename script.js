dc48c8ba93e13875fd35f7b4c4e9c2110bc8040a
Fatai Abiodun M. <fataimonsuru2020@gmail.com>
2024-10-07 13:47:03 +0100

    getting all my btn and assigning event

--- script.js
+++ script.js
@@ -121,5 +121,23 @@ class ShoppingCart {
     const currentProductCountSpan = document.getElementById(
       `product-count-for-id${id}`
     );
+
+    currentProductCount > 1
+      ? (currentProductCountSpan.textContent = `${currentProductCount}x`)
+      : (productsContainer.innerHTML += `
+      <div id="dessert${id}" class="product">
+        <p>
+          <span class="product-count" id="product-count-for-id${id}"></span>${name}
+        </p>
+        <p>${price}</p>
+      </div>
+      `);
   }
 }
+
+const cart = new ShoppingCart();
+const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
+
+[...addToCartBtns].forEach((btn) => {
+  btn.addEventListener("click", (event) => {});
+});
